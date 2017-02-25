d73c1ffb9ab459f97cea189cad03f1cc184f06aa
wolfsyntax <wolfsyntax@yahoo.com>
2017-02-25 23:59:16 +0800

    Create proba.cpp

---
+++ apc2017/proba.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int test;
+
+	freopen("proba.in","r",stdin);
+
+	int t;
+	cin >> t; 
+
+	while(t--){
+		
+		int n, sum, s, total = 0, needs = 0; cin >> n;
+		int coins[n];
+
+		for(int i = 0; i < n; i++){
+			cin >> coins[i];
+			total += coins[i];
+		}
+		
+		cin >> sum; s = sum;
+		if(total >= sum)
+			for(int i = 0; i < n; i++){
+				
+				if(coins[i] == s){
+					needs = 1;
+					break;
+				}
+
+				
+				if(sum > coins[i]){
+					needs += coins[i];
+					sum -= coins[i];
+				}
+
+				if(coins[i] >= sum){
+					needs = 0;
+					needs = s;
+					coins[0];
+				}
+
+			}
+		if(needs == 0) cout << "IMPOSSIBLE\n";
+		else{
+			cout << needs << endl;
+		}
+	}
+
+
+	return 0;
+}
